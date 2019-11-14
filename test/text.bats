0b9a61eae0865b57078f24bb97b4553e0ee53abe
Emmanuel Frecon <efrecon@gmail.com>
2019-11-14 17:19:08 +0100

    Adding few fns from sh bible+tests

--- test/text.bats
+++ test/text.bats
@@ -24,3 +24,91 @@ YUSH_LOG_LEVEL=warn
     passwd=$(yush_password 0)
     [ -z "$passwd" ]
 }
+
+@test "Find first sub-string" {
+    [ "$(yush_string_first "hello world" "world")" = "7" ]
+}
+
+@test "Find leading sub-string" {
+    [ "$(yush_string_first "hello world" "hello")" = "1" ]
+}
+
+@test "Find empty sub-string" {
+    [ "$(yush_string_first "hello world" "")" = "0" ]
+}
+
+@test "Find in empty sub-string" {
+    [ "$(yush_string_first "" "world")" = "0" ]
+}
+
+@test "Strip leading string" {
+    [ "$(yush_string_lstrip "hello world" "hello")" = " world" ]
+}
+
+@test "Strip empty leading string" {
+    [ "$(yush_string_lstrip "hello world" "")" = "hello world" ]
+}
+
+@test "Strip ending string" {
+    [ "$(yush_string_rstrip "hello world" "world")" = "hello " ]
+}
+
+@test "Strip empty ending string" {
+    [ "$(yush_string_rstrip "hello world" "")" = "hello world" ]
+}
+
+@test "Strip from string" {
+    [ "$(yush_string_strip "hello world hello" "hello")" = " world " ]
+}
+
+@test "Strip nothing from string" {
+    [ "$(yush_string_strip "hello world" "")" = "hello world" ]
+}
+
+@test "Trim beginning of string" {
+    [ "$(yush_string_ltrim "    hello world")" = "hello world" ]
+}
+
+@test "Trim ending of string" {
+    [ "$(yush_string_rtrim "hello world  ")" = "hello world" ]
+}
+
+@test "Trim string" {
+    [ "$(yush_string_trim "  hello world ")" = "hello world" ]
+}
+
+@test "Trim empty string" {
+    [ -z "$(yush_string_trim "")" ]
+}
+
+@test "Trim nothing" {
+    [ -z "$(yush_string_trim)" ]
+}
+
+@test "Trim string with only spaces" {
+    [ -z "$(yush_string_trim "        ")" ]
+}
+
+@test "Single spaces" {
+    [ "$(yush_string_trimall "    Hello,    World    ")" = "Hello, World" ]
+}
+
+@test "Single spaces from empty string" {
+    [ -z "$(yush_string_trimall "")" ]
+}
+
+@test "Single spaces from nothing" {
+    [ -z "$(yush_string_trimall)" ]
+}
+
+@test "Single spaces from only spaces" {
+    [ -z "$(yush_string_trimall "         ")" ]
+}
+
+@test "Split on sub-string" {
+    [ "$(yush_split "apple, bananas, oranges" ", " | wc -l)" = "3" ]
+}
+
+@test "Split on nothing" {
+    [ "$(yush_split "apple, bananas, oranges" "")" = "apple, bananas, oranges" ]
+}
--- text.sh
+++ text.sh
@@ -7,3 +7,115 @@ set -e
 yush_password() {
     tr -dc A-Za-z0-9 </dev/urandom 2>/dev/null | head -c"${1:-24}"; echo
 }
+
+yush_string_first() {
+    [ -z "$1" ] && echo "0" && return
+    [ -z "$2" ] && echo "0" && return
+    echo "$1"|awk "END{print index(\$0,\"$2\")}"
+}
+
+
+## Following functions adapted from https://github.com/dylanaraps/pure-sh-bible
+#
+# The MIT License (MIT)
+#
+# Copyright (c) 2019 Dylan Araps
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+###
+
+yush_string_rstrip() {
+    printf '%s\n' "${1%%$2}"
+}
+
+yush_string_lstrip() {
+    printf '%s\n' "${1##$2}"
+}
+
+yush_string_strip() {
+    yush_string_rstrip "$(yush_string_lstrip "$1" "$2")" "$2"
+}
+
+yush_string_ltrim() {
+    # Remove all leading white-space.
+    # '${1%%[![:space:]]*}': Strip everything but leading white-space.
+    # '${1#${XXX}}': Remove the white-space from the start of the string.
+    printf '%s\n' "${1#${1%%[![:space:]]*}}"
+}
+
+yush_string_rtrim() {
+    # Remove all trailing white-space.
+    # '${trim##*[![:space:]]}': Strip everything but trailing white-space.
+    # '${trim#${XXX}}': Remove the white-space from the end of the string.
+    printf '%s\n' "${1%${1##*[![:space:]]}}"
+}
+
+yush_string_trim() {
+    yush_string_rtrim "$(yush_string_ltrim "$1" "$2")" "$2"
+}
+
+# shellcheck disable=SC2086,SC2048
+yush_string_trimall() {
+    # Disable globbing to make the word-splitting below safe.
+    set -f
+
+    # Set the argument list to the word-splitted string.
+    # This removes all leading/trailing white-space and reduces
+    # all instances of multiple spaces to a single ("  " -> " ").
+    set -- $*
+
+    # Print the argument list as a string.
+    printf '%s\n' "$*"
+
+    # Re-enable globbing.
+    set +f
+}
+
+yush_split() {
+    [ -z "$2" ] && echo "$1" && return
+
+    # Disable globbing.
+    # This ensures that the word-splitting is safe.
+    set -f
+
+    # Store the current value of 'IFS' so we
+    # can restore it later.
+    old_ifs=$IFS
+
+    # Change the field separator to what we're
+    # splitting on.
+    IFS=$2
+
+    # Create an argument list splitting at each
+    # occurance of '$2'.
+    #
+    # This is safe to disable as it just warns against
+    # word-splitting which is the behavior we expect.
+    # shellcheck disable=2086
+    set -- $1
+
+    # Print each list value on its own line.
+    printf '%s\n' "$@"
+
+    # Restore the value of 'IFS'.
+    IFS=$old_ifs
+
+    # Re-enable globbing.
+    set +f
+}
