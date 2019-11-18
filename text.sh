bd5a591ad16c627b92c81462e9dc443f103d08aa
Emmanuel Frecon <emmanuel.frecon@yanzinetworks.com>
2019-11-18 13:05:02 +0100

    Fix for Mac

--- text.sh
+++ text.sh
@@ -5,7 +5,7 @@ set -e
 # Generate a good and shell-safe password, length is passed as an argument
 # (defaults to 24 chars).
 yush_password() {
-    tr -dc A-Za-z0-9 </dev/urandom 2>/dev/null | head -c"${1:-24}"; echo
+    LC_ALL=C tr -dc A-Za-z0-9 </dev/urandom 2>/dev/null | head -c"${1:-24}"; echo
 }
 
 yush_string_first() {
