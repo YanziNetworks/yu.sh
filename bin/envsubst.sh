e6b4768e9334dab36f5362c31b401da6df4ca7be
Emmanuel Frecon <efrecon@gmail.com>
2020-12-03 16:42:40 +0100

    Improve envsubst impl. and create bin for it

---
+++ bin/envsubst.sh
@@ -0,0 +1,84 @@
+#!/bin/sh
+
+set -eu
+
+# Find yu.sh (tuned for Docker) and load modules
+ROOT_DIR=$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )
+YUSH_DIR="$ROOT_DIR/.."
+# shellcheck source=yu.sh/log.sh disable=SC1091
+. "$YUSH_DIR/log.sh"
+# shellcheck source=yu.sh/log.sh disable=SC1091
+. "$YUSH_DIR/multi-arch.sh"
+
+# Output destination, empty for stdout.
+ENVSUBST_OUTPUT=${ENVSUBST_OUTPUT:-/dev/stdout}
+
+# Print usage on stderr and exit
+usage() {
+    [ -n "$1" ] && echo "$1" >&2
+    exitcode="${2:-1}"
+    cat <<USAGE >&2
+
+Description:
+
+  $YUSH_APPNAME will substitute the value of environment variables.
+
+Usage:
+  $(basename "$0") [-option arg --long-option(=)arg] [--] [path]...
+
+  where all dash-led options are as follows (long options can be followed by
+  an equal sign):
+    -v | --verbose   Verbosity level
+
+Details:
+  This is a pure shell implementation of envsubst in a eval-safe manner. It
+  will substitute the content of environment variables in all files passed as
+  a parameter to the destination path (or the stdout if no path was given).
+  When no filepath are passed, input will be taken from stdin.
+
+  Note that it does not support default values such as ${MYVAR:-default}
+  constructs.
+
+USAGE
+    exit "$exitcode"
+}
+
+while [ $# -gt 0 ]; do
+    case "$1" in
+        -v | --verbose)
+            # shellcheck disable=SC2034
+            YUSH_LOG_LEVEL=$2; shift 2;;
+        --verbose=*)
+            # shellcheck disable=SC2034
+            YUSH_LOG_LEVEL="${1#*=}"; shift 1;;
+
+        -o | --output)
+            # shellcheck disable=SC2034
+            ENVSUBST_OUTPUT=$2; shift 2;;
+        --output=*)
+            # shellcheck disable=SC2034
+            ENVSUBST_OUTPUT="${1#*=}"; shift 1;;
+
+        --non-interactive | --no-colour | --no-color)
+            # shellcheck disable=SC2034
+            YUSH_LOG_COLOUR=0; shift 1;;
+
+        -h | --help)
+            usage "" 0;;
+
+        --)
+            shift; break;;
+        -*)
+            usage "Unknown option: $1 !";;
+        *)
+            break;;
+    esac
+done
+
+if [ "$#" -gt "0" ]; then
+  for SRC in "$@"; do
+    yush_envsubst < "$SRC" > "$ENVSUBST_OUTPUT"
+  done
+else
+  yush_envsubst > "$ENVSUBST_OUTPUT"
+fi
--- multi-arch.sh
+++ multi-arch.sh
@@ -20,37 +20,27 @@ yush_envsubst() {
     if [ "$#" -gt "0" ]; then
         printf %s\\n "$1" | yush_envsubst
     else
-        # Prepare a sed script that will replace all occurrences of the known
-        # environment variables by their value
-        _sed=$(mktemp)
-        while IFS='=' read -r var val; do
-            for separator in ! ~ ^ % \; /; do
-                if ! printf %s\\n "$val" | grep -qo "$separator"; then
-                    printf 's%s\x04%s%s%s%sg\n' \
-                        "$separator" "$var" "$separator" "$val" "$separator" >> "$_sed"
-                    break
-                fi
-            done
-        done <<EOF
-$(env|grep -E '^[0-9[:upper:]][0-9[:upper:]_]*=')
-EOF
-
         while IFS= read -r line || [ -n "$line" ]; do  # Read, incl. non-empty last line
-            # Transpose all chars that could trigger an expansion to control
-            # characters, and perform expansion using the script above for pure
-            # variable substitutions. Once done, transpose only the ${ back to
-            # what they should (and escape the double quotes)
-            _line=$(printf %s\\n "$line" |
-                        tr '`([$' '\1\2\3\4' |
-                        sed -f "$_sed" |
-                        sed -e 's/\x04{/${/g' -e 's/"/\\\"/g')
+            # Transform everything that looks like an environment variable (all
+            # caps, no first digit) to its {} equivalent. This transform
+            # understands \$ quoting, but will fail within single quotes. Then
+            # escape ALL characters that could trigger an expansion.
+            IFS= read -r _lineEscaped << EOF
+$(printf %s "$line" | sed -E -e 's/([^\\])\$([A-Z_][A-Z0-9_]*)/\1\${\2}/g' -e 's/^\$([A-Z_][A-Z0-9_]*)/\${\1}/g' | tr '`([$' '\1\2\3\4')
+EOF
+            # ... then selectively reenable ${ references
+            _lineEscaped=$(printf %s\\n "$_lineEscaped" | sed -e 's/\x04{/${/g' -e 's/"/\\\"/g')
+            # Disable unset errors to ensure we output something (with empty
+            # value for var)
+            _oldstate=$(set +o); set +u
             # At this point, eval is safe, since the only expansion left is for
-            # ${} contructs. Perform the eval and convert back the control
-            # characters to the real chars.
-            eval "printf '%s\n' \"$_line\"" | tr '\1\2\3\4' '`([$'
+            # ${} contructs. Perform the eval, variables that do not exist will
+            # be replaced by an empty string.
+            _lineResolved=$(eval "printf '%s\n' \"$_lineEscaped\"")
+            # Restore set state
+            set +vx; eval "$_oldstate"
+            # and convert back the control characters to the real chars.
+            printf %s\\n "$_lineResolved" | tr '\1\2\3\4' '`([$'
         done
-
-        # Get rid of the temporary sed script
-        rm -f "$_sed"
     fi
 }
