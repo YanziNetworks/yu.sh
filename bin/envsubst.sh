fd120e97f4e29bbe5b19cfac33dcf9f789798211
Emmanuel Frecon <efrecon@gmail.com>
2020-12-03 16:52:29 +0100

    Arrange for amalgamation to work on envsubst.sh

--- bin/envsubst.sh
+++ bin/envsubst.sh
@@ -2,13 +2,15 @@
 
 set -eu
 
+### AMLG_START ../log.sh ../multi-arch.sh
 # Find yu.sh (tuned for Docker) and load modules
 ROOT_DIR=$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )
 YUSH_DIR="$ROOT_DIR/.."
 # shellcheck source=yu.sh/log.sh disable=SC1091
 . "$YUSH_DIR/log.sh"
-# shellcheck source=yu.sh/log.sh disable=SC1091
+# shellcheck source=yu.sh/multi-arch.sh disable=SC1091
 . "$YUSH_DIR/multi-arch.sh"
+### AMLG_END
 
 # Output destination, empty for stdout.
 ENVSUBST_OUTPUT=${ENVSUBST_OUTPUT:-/dev/stdout}
